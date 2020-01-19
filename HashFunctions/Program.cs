ef8782363626e8c942efb3bf2aebc2baf0e37f4c
Anton <anton.pushkin2@gmail.com>
2020-01-20 01:03:41 +0300

    first sample

--- HashFunctions/Program.cs
+++ HashFunctions/Program.cs
@@ -10,12 +10,82 @@ namespace HashFunctions
     {
         static void Main(string[] args)
         {
+            string password = "this is a password";
+            //int csum = ControlSum(password);
+            //Console.WriteLine(csum);
+            string aa = GetHash(password, 8);
+            Console.WriteLine(aa);
+
+            Console.ReadKey();
+
             List<string> list = GenerateStringList(100, 10);
             foreach (string str in list)
             {
                 Console.WriteLine(str);
             }
             Console.ReadKey();
+        }
+
+        private static string GetHash(string str, int hashLength)
+        {
+            string hash = "";
+
+            if (hashLength > 3)
+            {
+                int minLen = 2;
+                int realMinLen = 0;
+
+                int originalSalt = ControlSum(str);
+                int originalLengthStr = str.Length;
+
+                while (minLen <= hashLength)
+                    realMinLen = (minLen *= 2);
+
+                while (minLen < originalLengthStr)
+                    minLen *= 2;
+
+                if ((minLen - originalLengthStr) < minLen)
+                    minLen *= 2;
+
+                int addCount = minLen - originalLengthStr;
+
+                for (int i = 0; i < addCount; i++)
+                    str += ReceivingExistCodes(str[i] + str[i + 1]);
+
+                int maxSalt = ControlSum(str);
+                int maxLengthStr = str.Length;
+
+                while (str.Length != realMinLen)
+                {
+                    int center = str.Length / 2;
+                    hash = "";
+                    for (int i = 0; i < center; i++)
+                    {
+                        hash += ReceivingExistCodes(str[center - i] + str[center + 1]);
+                        str = hash;
+                    }
+                }
+
+                int rem = realMinLen - hashLength;
+                int countCompress = realMinLen / rem;
+                hash = "";
+
+                for (int i = 0; hash.Length < hashLength - 4; i++)
+                {
+                    if (i % countCompress == 0)
+                        hash += ReceivingExistCodes(str[i] + str[i + 1]);
+                    else
+                        hash += str[i];
+                }
+
+                hash += ReceivingExistCodes(originalSalt);
+                hash += ReceivingExistCodes(originalLengthStr);
+
+                hash += ReceivingExistCodes(maxSalt);
+                hash += ReceivingExistCodes(maxLengthStr);
+            }
+
+            return hash;
         }
 
         private static Random random = new Random();
@@ -34,6 +104,34 @@ namespace HashFunctions
                 list.Add(RandomString(stringSize));
             }
             return list;
+        }
+
+        public static int ControlSum(string str)
+        {
+            if (str.Length % 2 != 0)
+                str += "s";
+
+            int sum = 0;
+
+            for (int i = 0; i < str.Length; i += 2)
+            {
+                int mult = str[i] * str[i + 1];
+                int div = str[i] / str[i + 1];
+
+                sum += mult;
+                sum += div;
+            }
+            return sum;
+        }
+
+        public static int ReceivingExistCodes(int x)
+        {
+            x += 256;
+            while(!(((x <= 57) && (x >= 48)) || ((x <= 90) && (x <= 122)) || ((x <= 122) && (x >= 97))))
+                if (x < 48) { x += 24; }
+                else { x -= 47; }
+
+            return x;
         }
     }
 }
