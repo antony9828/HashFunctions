5d37b055ddd3819bd4f97ecd943ef9429fe458ec
Anton <anton.pushkin2@gmail.com>
2020-01-19 22:08:15 +0300

    added password generation

--- HashFunctions/Program.cs
+++ HashFunctions/Program.cs
@@ -10,6 +10,30 @@ namespace HashFunctions
     {
         static void Main(string[] args)
         {
+            List<string> list = GenerateStringList(100, 10);
+            foreach (string str in list)
+            {
+                Console.WriteLine(str);
+            }
+            Console.ReadKey();
+        }
+
+        private static Random random = new Random();
+        public static string RandomString(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnoprstuvwxyz0123456789";
+            return new string(Enumerable.Repeat(chars, length)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+
+        public static List<string> GenerateStringList(int listSize, int stringSize)
+        {
+            List<string> list = new List<string>();
+            for(int i = 0; i < listSize; i++)
+            {
+                list.Add(RandomString(stringSize));
+            }
+            return list;
         }
     }
 }
